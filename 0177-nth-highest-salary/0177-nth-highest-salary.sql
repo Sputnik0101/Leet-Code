3aad7a7eebab7bcd2d2a90ae53a48557e20dc2a8
Riyansha D <98273522+Sputnik0101@users.noreply.github.com>
2026-06-10 00:10:24 +0530

    Time: 416 ms (94.55%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN ( 
+    SELECT Salary 
+    FROM(
+    SELECT Salary,
+    DENSE_RANK() OVER (ORDER BY Salary DESC) AS rnk
+    FROM Employee
+  ) t
+  WHERE rnk = N
+  LIMIT 1
+      
+  );
+END
