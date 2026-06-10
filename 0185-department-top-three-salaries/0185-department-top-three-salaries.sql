1f27a9a10b8f73fe48b479abe61e4eef766bc906
Riyansha D <98273522+Sputnik0101@users.noreply.github.com>
2026-06-10 20:10:05 +0530

    Time: 997 ms (52.31%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,15 @@
+select d.name as Department, e.name as Employee, e.salary as Salary
+from(
+    select
+    name,
+    salary,
+    departmentId,
+    dense_rank() over(
+        partition by departmentId
+        order by salary desc
+    ) as rnk
+    from Employee
+) e
+join Department d
+on e.departmentId=d.id
+where rnk<= 3
