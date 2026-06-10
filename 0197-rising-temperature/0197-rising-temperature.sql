8e551dcf69d43aaed3fcd7274a5b68e3e75c6c94
Riyansha D <98273522+Sputnik0101@users.noreply.github.com>
2026-06-11 01:48:40 +0530

    Time: 463 ms (73.85%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,12 @@
-select w1.id from Weather w1 join Weather w2 on DATEDIFF(w1.recordDate, w2.recordDate)=1
-where w1.temperature>w2.temperature
+select id
+from(
+    select
+    id,
+    temperature,
+    recordDate,
+    LAG(temperature)  over (order by recordDate) as prev_temp,
+    LAG(recordDate)  over (order by recordDate) as prev_date
+    from Weather
+) w
+where temperature>prev_temp
+and DATEDIFF(recordDate,prev_date)=1
